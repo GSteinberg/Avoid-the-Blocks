18e20f062470209c6ee816b581cf4c21954f764c
GSteinberg <gsteinb1@binghamton.edu>
2016-12-23 18:35:33 -0500

    Direction of obstacles in game

---
+++ ObstaclesDirection.java
@@ -0,0 +1,10 @@
+public class ObstacleDirection {
+
+	public static final int startDirection = 0;
+	public static final int northWest = 1;
+	public static final int northEast = 2;
+	public static final int southWest = 3;
+	public static final int southEast = 4;
+	public static final int endDirection = 5;
+	
+}
